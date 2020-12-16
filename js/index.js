9816af7a00f37498fa8038294fcc0ec8534a67aa
MisterBrice <bgrunberg@hotmail.fr>
2020-12-16 15:32:33 +0100

    js creation post it

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
-var balise=document.querySelector('#jsload');
-balise.style.backgroundColor="green";
-balise.innerHTML="JS CHARGE";
+window.addEventListener('load', function (evt) {
+    initialisationJS();
+    document.querySelector('form').addEventListener('submit', formSubmited);    
+});
+
+function initialisationJS() {
+    var balise = document.querySelector('#jsload');
+    balise.style.backgroundColor = "green";
+    balise.innerHTML = "JS CHARGE";
+}
+
+initialisationJS();
+
+function formSubmited(event) {
+    event.preventDefault();
+    
+    createPostit(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].value);
+}
+/**
+ * Création d'un post-it
+ * @param {*} titre 
+ * @param {*} date 
+ * @param {*} heure 
+ * @param {*} description 
+ */
+function createPostit (titre, date, heure, description) {
+    var postitTmp = document.createElement('div');
+    postitTmp.classList.add('postit');
+    postitTmp.innerHTML='<div class="postit-titre">' + titre + '</div> \
+    date : <span class="datetime">' + date + '</span> heure : <span class="datetime">' + heure + '</span> \
+    <h2>Description :</h2>' + description;
+
+    var listPostit = document.querySelector('#list');
+    listPostit.append(postitTmp);
+}
+
