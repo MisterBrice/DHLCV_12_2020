75e459df257747d83f533a2d264f5050afb51694
MisterBrice <bgrunberg@hotmail.fr>
2020-12-18 08:36:00 +0100

    suppression post-it

--- js/index.js
+++ js/index.js
@@ -13,8 +13,13 @@ initialisationJS();
 
 function formSubmited(event) {
     event.preventDefault();
-    
-    createPostit(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].value);
+
+    var monFormulaire = document.forms['editor-form'];
+    // var dateFormated = moment (monFormulaire['date'].value, 'DD MM YYYY');
+
+    createPostit(monFormulaire['title'].value, monFormulaire['date'].value, monFormulaire['time'].value, monFormulaire['description'].value);
+        
+    // createPostit(event.target[0].value, event.target[1].value, event.target[2].value, event.target[3].value);
 }
 /**
  * Création d'un post-it
@@ -26,11 +31,18 @@ function formSubmited(event) {
 function createPostit (titre, date, heure, description) {
     var postitTmp = document.createElement('div');
     postitTmp.classList.add('postit');
-    postitTmp.innerHTML='<div class="postit-titre">' + titre + '</div> \
+    postitTmp.innerHTML=' <div class="close"><img src="img/icon_close.png"/></div><div class="postit-titre">' + titre + '</div> \
     date : <span class="datetime">' + date + '</span> heure : <span class="datetime">' + heure + '</span> \
     <h2>Description :</h2>' + description;
-
+    
+    postitTmp.querySelector('.close img').addEventListener('click', deletePostit);
+    
     var listPostit = document.querySelector('#list');
     listPostit.append(postitTmp);
+}
+
+function deletePostit (event) {
+    console.log('Suppression Post-it'), event;
+    event.currentTarget.parentElement.parentElement.remove();
 }
 
